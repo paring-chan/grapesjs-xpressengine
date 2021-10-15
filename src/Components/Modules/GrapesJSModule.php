12e2b26e4c4160168d9a8c4afa4c829b09bb16f7
pikokr <parangee9706@gmail.com>
2021-10-15 21:05:20 +0900

    initial commit

---
+++ src/Components/Modules/GrapesJSModule.php
@@ -0,0 +1,38 @@
+<?php
+
+class GrapesJSModule extends \Xpressengine\Menu\AbstractModule {
+    public function createMenuForm()
+    {
+        // TODO: Implement createMenuForm() method.
+    }
+
+    public function storeMenu($instanceId, $menuTypeParams, $itemParams)
+    {
+        // TODO: Implement storeMenu() method.
+    }
+
+    public function editMenuForm($instanceId)
+    {
+        // TODO: Implement editMenuForm() method.
+    }
+
+    public function updateMenu($instanceId, $menuTypeParams, $itemParams)
+    {
+        // TODO: Implement updateMenu() method.
+    }
+
+    public function summary($instanceId)
+    {
+        // TODO: Implement summary() method.
+    }
+
+    public function deleteMenu($instanceId)
+    {
+        // TODO: Implement deleteMenu() method.
+    }
+
+    public function getTypeItem($id)
+    {
+        // TODO: Implement getTypeItem() method.
+    }
+}
