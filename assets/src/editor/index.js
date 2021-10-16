ec7d06b4e59d721d4ce69386cecdc7dabafb052b
pikokr <parangee9706@gmail.com>
2021-10-16 15:37:44 +0900

    prettier

--- assets/src/editor/index.js
+++ assets/src/editor/index.js
@@ -1,25 +1,28 @@
 import grapesjs from "grapesjs"
-import PresetWebpage from 'grapesjs-preset-webpage'
-import axios from "axios";
+import PresetWebpage from "grapesjs-preset-webpage"
+import axios from "axios"
 
 const editor = grapesjs.init({
-    container: "#gjs",
-    storageManager: false,
-    fromElement: true,
-    plugins: [PresetWebpage]
+  container: "#gjs",
+  storageManager: false,
+  fromElement: true,
+  plugins: [PresetWebpage],
 })
 
-editor.on('load', () => {
-    const panelManager = editor.Panels
-    panelManager.addButton('options', {
-        id: 'xe_save',
-        command: async (editor) => {
-            await axios.put(window.location.pathname, new URLSearchParams({
-                html: editor.getHtml(),
-                css: editor.getCss()
-            }))
-            editor.Modal.setTitle('저장').setContent('저장 완료').open()
-        },
-        label: '저장하기'
-    })
+editor.on("load", () => {
+  const panelManager = editor.Panels
+  panelManager.addButton("options", {
+    id: "xe_save",
+    command: async (editor) => {
+      await axios.put(
+        window.location.pathname,
+        new URLSearchParams({
+          html: editor.getHtml(),
+          css: editor.getCss(),
+        })
+      )
+      editor.Modal.setTitle("저장").setContent("저장 완료").open()
+    },
+    label: "저장하기",
+  })
 })
--- assets/webpack.mix.js
+++ assets/webpack.mix.js
@@ -1,8 +1,10 @@
 const mix = require("laravel-mix")
-const path = require("path");
+const path = require("path")
 
 mix.js("src/editor/index.js", "editor/js/index.js")
 
 mix.sass("src/editor/index.scss", "dist/editor/css/index.css")
 
-mix.setPublicPath(path.join(__dirname, 'dist')).setResourceRoot('/plugins/grapes_js/assets/dist')
+mix
+  .setPublicPath(path.join(__dirname, "dist"))
+  .setResourceRoot("/plugins/grapes_js/assets/dist")
